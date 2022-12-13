89c9c58805ded4cdbc28f22306221c49b5bc060d
crenaz <crenaz@gmail.com>
2022-12-13 15:16:28 -0400

    added ml5

--- sketch.js
+++ sketch.js
@@ -13,16 +13,13 @@ console.log('ml5 version:', ml5.version);
 // When the model is loaded
 function modelReady() {
 	console.log('Model is ready!');
-	mobileNet.predict(puffin, gotResults)
+	mobileNet.predict(gotResults);
   }
 
 function gotResults(error, results) {
 	if (error) {
 		console.error(error);
 	} else {
-		console.log(typeof results[0]);
-		console.log(results[0]);
-		
 		let rstls = (results[0]);
 		// let valores = results[0];
 		// console.log(valores);
@@ -35,36 +32,59 @@ function gotResults(error, results) {
 		// 	console.log(`${key}: ${valores[key]}`);
 		// }
 
-
-		let list = document.getElementById("myList");
-
+		let alist = document.getElementById("aList");
 		for(const [key, value] of Object.entries(rstls)) {
 			let li = document.createElement("li");
 			li.innerText = (`${key}: ${value}`);
-			list.appendChild(li);
+			alist.appendChild(li);
 		}
-		let birdName = Object.values(rstls.label);
+		let bList = document.getElementById("bList");
+		for(const [key, value] of Object.entries(results[1])) {
+			let li = document.createElement("li");
+			li.innerText = (`${key}: ${value}`);
+			bList.appendChild(li);
+		}
+		let cList = document.getElementById("cList");
+		for(const [key, value] of Object.entries(results[2])) {
+			let li = document.createElement("li");
+			li.innerText = (`${key}: ${value}`);
+			cList.appendChild(li);
+		}
 
+		let birdName = Object.values(rstls.label);
 		fill(0);
 		textSize(64);
 		text(birdName, 250, height - 100);
-		createP(results);
+
+		//doesn't work, as it shows 'undefined'
+		let prob = results[0].probability;
+		console.log(prob);
+		console.log(typeof prob);
+		createP(prob);
+		// call self again
+		mobileNet.predict(gotResults);
+
 	}
 }
 
-function imageReady() {
-	image(puffin, 0, 0, width, height);
-}
+// function imageReady() {
+// 	image(puffin, 0, 0, width, height);
+// }
 
 function setup(){
 	createCanvas(640, 480);
-	//video = createCapture(VIDEO);
-	puffin = createImg('images/puffin.jpg', 'a puffin bird', imageReady);
-	puffin.hide();
+	video = createCapture(VIDEO);
+	//puffin = createImg('images/puffin.jpg', 'a puffin bird', imageReady);
+	video.hide();
 	background(200);
 	textSize(width / 3);
 	textAlign(CENTER, CENTER);
-	mobileNet = ml5.imageClassifier('MobileNet', modelReady);
+	mobileNet = ml5.imageClassifier('MobileNet', video, modelReady);
+	// the version number does not get printed out! :<
+	createP('ml5 version:', `${ml5.version}`);
 }
 
-function draw(){ }
+function draw(){
+	image(video, 0, 0);
+
+}
